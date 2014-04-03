ec54fb53cdc5d74b83629a75138f9c672351713b
alex <alexander.vjatkin@gmail.com>
2014-04-03 22:26:24 +0400

    change2

--- src/com/xogame/Main.java
+++ src/com/xogame/Main.java
@@ -5,11 +5,31 @@ import java.io.Console;
 
 
 public class Main {
+
+    public static Gamefield f1 = new Gamefield();
+
     public static void main(String[] args) {
 
-        Gamefield f1 = new Gamefield();
         f1.eraseField();
         f1.showField();
+        playermove('X', 0, 1);
+        f1.showField();
+        playermove('X', 1, 1);
+        f1.showField();
+        playermove('O', 1, 1);
+        f1.showField();
+
+
+
+
+    }
+
+    public static void playermove(char pl, int x, int y) {
+
+        switch(pl) {
+            case 'X' : { f1.set(pl, x, y); break;}
+            case 'O' : { f1.set(pl, x, y); break;}
+        }
 
     }
 }
