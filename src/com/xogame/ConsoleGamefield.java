ca9ae8d3a65c3781b9a7079ea8fef75a8b128585
alex <alexander.vjatkin@gmail.com>
2014-05-13 16:11:05 +0400

    rereremastered

--- src/com/xogame/ConsoleGamefield.java
+++ src/com/xogame/ConsoleGamefield.java
@@ -1,73 +1,77 @@
 package com.xogame;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ConsoleGamefield implements GameInterface {
+public class ConsoleGamefield extends GameFieldMain {
 
-    @Override
-    public void setSizeField(int size) {
-        System.out.println("Введите размер поля");
-        int SIZE_FIELD = size;
+
+    public void setSizeField() {
+        boolean fault = true;
+        while (fault) {
+            System.out.print("Введите размерность игрового поля: ");
+            Scanner scanner = new Scanner(System.in);
+            try {
+                this.sizeField = scanner.nextInt();
+                fault = false;
+            }catch(Exception er){
+                System.out.println("Неверный ввод, попробуйте снова!!");
+            }
+        }
     }
-
-    private static final char DEFAULT_CHAR = ' ';
-    public static char[][] consoleField = new char[SIZE_FIELD][SIZE_FIELD];
-    private static final int MAX_COUNTER = SIZE_FIELD*SIZE_FIELD;
-
 
     @Override
     public void showField() {
+        for (int i = 0; i < sizeField; i++) {
+            System.out.print("  " + (i + 1) );
+        }
         System.out.println();
-        System.out.println("  1  2  3");
-        for (int j = 0; j < SIZE_FIELD; j++) {
-            System.out.print(j + 1);
-            for (int i = 0; i < SIZE_FIELD; i++) {
-                System.out.print("[" + consoleField[j][i] + "]");
+        for (int i = 0; i < sizeField; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < sizeField; j++) {
+                switch(field.get(j).get(i)) {
+                    case empty: {
+                        System.out.print("[ ]"); break;
+                    }
+                    case x:{
+                        System.out.print("[X]"); break;
+                    }
+                    case o: {
+                        System.out.print("[O]"); break;
+                    }
+                }
             }
             System.out.println();
         }
-    }
-
-    @Override
-    public void eraseField() {
-        initializeField();
-        for (int j = 0; j < SIZE_FIELD; j++) {
-            for (int i = 0; i < SIZE_FIELD; i++) {
-                consoleField[i][j] = DEFAULT_CHAR;
-            }
-        }
+        System.out.println();
     }
 
     @Override
     public void playersMove(Value player) {
-        boolean success = false;
-        while (!success) {
+        boolean fault = true;
+        while (fault) {
             Scanner in = new Scanner(System.in);
-            System.out.println(player + ": Введите число от 1 до " + SIZE_FIELD);
+            System.out.println(player + ": Введите число от 1 до " + sizeField);
             try {
                 int x = in.nextInt()-1;
                 int y = in.nextInt()-1;
-                if (isEmpty(x, y)) {
-                    set(player, x, y);
-                    switch (player) {
-                        case x : consoleField[x][y] = 'X'; break;
-                        case o : consoleField[x][y] = 'O'; break;
-                    }
-                    success = true;
-                } else System.out.println("Неверный ввод, попробуйте ещё раз, " + player + "!!");
+                if (isCellEmpty(x, y)) {
+                    setFieldCell(player, x, y);
+                    fault = false;
+                } else System.out.println("Поле занято, попробуйте ещё раз, " + player + "!!");
             } catch (Exception e) {
                 System.out.println("Неверный ввод, попробуйте ещё раз, " + player + "!!");
             }
         }
     }
 
-    @Override
+
     public void messageWin(Value player) {
         System.out.println( player + ", Вы выиграли!!");
     }
 
-    @Override
+
     public void messageDraw() {
         System.out.println("Ничья!!");
     }
--- src/com/xogame/Main.java
+++ src/com/xogame/Main.java
@@ -2,30 +2,27 @@ package com.xogame;
 
 public class Main {
 
-    public static GameInterface f2 = new ConsoleGamefield();
+    public static GameFieldMain f2 = new ConsoleGamefield();
 
     public static void main(String[] args) {
         int counter = 0;
         Value pl = Value.o;
-        f2.setSizeField(f2.SIZE_FIELD);
-        f2.eraseField();
+        f2.setSizeField();
+        f2.initField();
         f2.showField();
 
-        while(!CheckWin.check(pl) &&  counter < f2.SIZE_FIELD*f2.SIZE_FIELD) {
-            pl = changePlayer(pl);
+        while(!f2.check(pl) &&  counter < f2.sizeField*f2.sizeField) {
+            pl = Value.changePlayer(pl);
             f2.playersMove(pl);
             f2.showField();
-            if (CheckWin.check(pl)) f2.messageWin(pl);
+            if (f2.check(pl)) f2.messageWin(pl);
             counter++;
         }
-        if ((counter == (f2.SIZE_FIELD * f2.SIZE_FIELD)) && !CheckWin.check(pl)) f2.messageDraw();
-    }
+        if ((counter == (f2.sizeField * f2.sizeField)) && !f2.check(pl)) f2.messageDraw();
+//    }
 
 
 
-    private static Value changePlayer(Value player) {
-        if (player == Value.o) player = Value.x; else player = Value.o;
-        return player;
     }
 }
 
